131fd180607828b04c032c144c42c9614198f052
CDOOR <CDOOR@CHUGIN>
2025-11-12 00:13:45 +0500

    Task5

---
+++ Tyuiu.ChuginNM.Sprint4.Task5.V28.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task5.V28.Lib
+{
+    public class DataService : ISprint4Task5V28
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    matrix[i, j] = (matrix[i, j] < 0) ? 0 : matrix[i, j];
+                }
+            }
+
+            return matrix;
+        }
+    }
+}
---
+++ Tyuiu.ChuginNM.Sprint4.Task5.V28/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.ChuginNM.Sprint4.Task5.V28.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task5.V28
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] array = new int[5, 5];
+            Random random = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    array[i, j] = random.Next(-6, 3);
+                }
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(array));
+        }
+    }
+}
+
+    
