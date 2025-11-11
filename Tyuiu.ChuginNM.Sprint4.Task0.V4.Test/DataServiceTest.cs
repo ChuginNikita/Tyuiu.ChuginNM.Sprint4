c3651c94e6e15c6e6390ba0a8fcc76d6281a7c4c
CDOOR <CDOOR@CHUGIN>
2025-11-11 18:13:51 +0500

    Task0

---
+++ Tyuiu.ChuginNM.Sprint4.Task0.V4.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ChuginNM.Sprint4.Task0.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+        }
+    }
+}
