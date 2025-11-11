6c686d17bbf4a818f5298ffa96460dccee0f6c71
CDOOR <CDOOR@CHUGIN>
2025-11-12 01:04:18 +0500

    Task7

---
+++ Tyuiu.ChuginNM.Sprint4.Task7.V12.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task7.V12.Lib
+{
+    public class DataService : ISprint4Task7V12
+    {
+        public int Calculate(int n, int m, string value)
+        { 
+            int[,] array = new int[n, m];
+
+
+            int index = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    array[i, j] = int.Parse(value[index].ToString());
+                    index++;
+                }
+            }
+
+            int res = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (array[i, j] % 2 != 0)
+                    {
+                        res += array[i, j];
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
--- Tyuiu.ChuginNM.Sprint4.Task7.V12/Program.cs
+++ Tyuiu.ChuginNM.Sprint4.Task7.V12/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.ChuginNM.Sprint4.Task7.V12.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task7.V12
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
+            Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            string input = "658932125478";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(3, 4, input));
+        }
+    }
+}
