f40a89ea7f68da8f0a35b4021bddb6b65bf164dd
CDOOR <CDOOR@CHUGIN>
2025-11-12 00:41:20 +0500

    Task6

---
+++ Tyuiu.ChuginNM.Sprint4.Task6.V19.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task6.V19.Lib
+{
+    public class DataService : ISprint4Task6V19
+    {
+        public int Calculate(string[] array)
+        {
+            int res = 0;
+            foreach (var a in array)
+            {
+                if (a.Length > 5) { res++; }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.ChuginNM.Sprint4.Task6.V19/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.ChuginNM.Sprint4.Task6.V19.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task6.V19
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Двумерные массивы                                                 *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #28                                                             *");
+            Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            string[] input = { "Chrome", "Firefox", "Safari", "Opera", "Edge", "Internet Explorer", "Brave" };
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(input));
+        }
+    }
+}
