b5de824c1e40699ff2225290331a62bb783c6f3d
CDOOR <CDOOR@CHUGIN>
2025-11-11 18:12:02 +0500

    Task0

--- Tyuiu.ChuginNM.Sprint4.Task0.V4/Program.cs
+++ Tyuiu.ChuginNM.Sprint4.Task0.V4/Program.cs
@@ -4,20 +4,21 @@ namespace Tyuiu.ChuginNM.Sprint4.Task0.V4
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             DataService ds = new DataService();
-            Console.Title = "Спринт #4 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
+
+            Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
             Console.WriteLine("* Задание #0                                                              *");
             Console.WriteLine("* Вариант #4                                                              *");
             Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
-            Console.WriteLine("* значениями от 0 до 9, подсчитать произведение четных элементов массива. *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
             Console.WriteLine("***************************************************************************");
@@ -25,9 +26,9 @@ namespace Tyuiu.ChuginNM.Sprint4.Task0.V4
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
             Console.WriteLine("***************************************************************************");
+
             int[] array = [5, 3, 7, 1, 3, 9, 8, 7, 9, 4];
-            ds.GetMultEvenArrEl(array);
+            Console.WriteLine(ds.GetMultEvenArrEl(array));
         }
     }
-
 }
