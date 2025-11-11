43a55786ef2671820689ebaa4a0f9678269ca615
CDOOR <CDOOR@CHUGIN>
2025-11-11 20:04:48 +0500

    Task2

---
+++ Tyuiu.ChuginNM.Sprint4.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.ChuginNM.Sprint4.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+        }
+    }
+}
---
+++ Tyuiu.ChuginNM.Sprint4.Task2.V26/Program.cs
@@ -0,0 +1,40 @@
+﻿using Tyuiu.ChuginNM.Sprint4.Task2.V26.Lib;
+
+namespace Tyuiu.ChuginNM.Sprint4.Task2.V26 {
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Чугин Н. М. | АСОиУб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы                                                *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #26                                                             *");
+            Console.WriteLine("* Выполнил: Чугин Никита Максимович | АСОиУб-25-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение                         *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Random ran = new Random();
+            int[] array = new int[14];
+            for (int i = 0; i < 14; i++)
+            {
+                array.Append(ran.Next(1, 8));
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate(array));
+        }
+    }
+
+}
